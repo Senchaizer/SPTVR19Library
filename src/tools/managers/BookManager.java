6e2ca312e7349bcf9f7873136e37e7be440dcaee
Zahhar <Zahhar.Pertsev@ivkhk.ee>
2020-10-14 15:08:51 +0300

    Решена задача входа в систему

--- src/tools/managers/BookManager.java
+++ src/tools/managers/BookManager.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package tools.managers;
 
 import entity.Book;
@@ -41,3 +46,5 @@ public class BookManager {
             }
         }
     }
+    
+}
